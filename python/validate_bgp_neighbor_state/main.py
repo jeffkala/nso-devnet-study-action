93fd7b801c99c3870730d36894d87baa98ca89b0
Jeff Kala <jeff.kala@networktocode.com>
2023-06-07 16:36:05 -0600

    initial updates to action service

--- python/validate_bgp_neighbor_state/main.py
+++ python/validate_bgp_neighbor_state/main.py
@@ -1,7 +1,5 @@
 # -*- mode: python; python-indent: 4 -*-
 import ncs
-
-# from ncs.application import Service
 from ncs.dp import Action
 
 
@@ -14,69 +12,21 @@ class DoubleAction(Action):
         self.log.info('action name: ', name)
         self.log.info('action input.device: ', input.device)
         self.log.info('action input.bgp_neighbor_addr: ', input.bgp_neighbor_addr)
+        query = trans.get_oper()
+        # Specify the XPath to retrieve BGP neighbor status
+        query.xpath(f'/devices/device[name="{input.device}"]/live-status/bgp/neighbor')
 
-        # Updating the output data structure will result in a response
-        # being returned to the caller.
-        # output.result = input.number * 2
-
-
-# ------------------------
-# SERVICE CALLBACK EXAMPLE
-# ------------------------
-# class ServiceCallbacks(Service):
-
-#     # The create() callback is invoked inside NCS FASTMAP and
-#     # must always exist.
-#     @Service.create
-#     def cb_create(self, tctx, root, service, proplist):
-#         self.log.info('Service create(service=', service._path, ')')
-
-
-    # The pre_modification() and post_modification() callbacks are optional,
-    # and are invoked outside FASTMAP. pre_modification() is invoked before
-    # create, update, or delete of the service, as indicated by the enum
-    # ncs_service_operation op parameter. Conversely
-    # post_modification() is invoked after create, update, or delete
-    # of the service. These functions can be useful e.g. for
-    # allocations that should be stored and existing also when the
-    # service instance is removed.
-
-    # @Service.pre_modification
-    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service premod(service=', kp, ')')
-
-    # @Service.post_modification
-    # def cb_post_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service postmod(service=', kp, ')')
-
+        # Execute the query and retrieve the result
+        result = query.apply()
+        self.log.info('result: ', result)
 
 # ---------------------------------------------
 # COMPONENT THREAD THAT WILL BE STARTED BY NCS.
 # ---------------------------------------------
 class Main(ncs.application.Application):
     def setup(self):
-        # The application class sets up logging for us. It is accessible
-        # through 'self.log' and is a ncs.log.Log instance.
         self.log.info('Main RUNNING')
-
-        # Service callbacks require a registration for a 'service point',
-        # as specified in the corresponding data model.
-        #
-        # self.register_service('validate-bgp-neighbor-state-servicepoint', ServiceCallbacks)
-
-        # When using actions, this is how we register them:
-        #
         self.register_action('validate-bgp-neighbor-state-action', DoubleAction)
 
-        # If we registered any callback(s) above, the Application class
-        # took care of creating a daemon (related to the service/action point).
-
-        # When this setup method is finished, all registrations are
-        # considered done and the application is 'started'.
-
     def teardown(self):
-        # When the application is finished (which would happen if NCS went
-        # down, packages were reloaded or some error occurred) this teardown
-        # method will be called.
-
         self.log.info('Main FINISHED')
