c4dd5746dc4d59cd40eba7fef76b9e722a3889d6
Jeff Kala <jeff.kala@networktocode.com>
2023-06-07 22:25:53 -0600

    go into nested live status to ios bgp method

--- python/validate_bgp_neighbor_state/main.py
+++ python/validate_bgp_neighbor_state/main.py
@@ -9,22 +9,13 @@ from ncs.dp import Action
 class DoubleAction(Action):
     @Action.action
     def cb_action(self, uinfo, name, kp, input, output, trans):
-
-        # EXAMPLE FROM SHELL
-        # with ncs.maapi.Maapi() as m:
-        #     with ncs.maapi.Session(m, 'admin', 'admin'):
-        #         with m.start_write_trans(ncs.RUNNING) as t:
-        #             node = ncs.maagic.get_node(t, '/ncs:devices/device{router-01}/live-status/ios-stats:bgp/ipv4/unicast/neighbors{1.1.1.2}/ios-stats:bgp-state')
-        #             print(node)
-
         self.log.info('action name: ', name)
         self.log.info('action input.device: ', input.device)
         self.log.info('action input.bgp_neighbor_addr: ', input.bgp_neighbor_addr)
-        root = ncs.maagic.get_root(trans)
-        device = root.devices.device[input.device]
-        result = ncs.maagic.get_node(trans, '/ncs:devices/device{router-01}/live-status/ios-stats:bgp/ipv4/unicast/neighbors{1.1.1.2}/ios-stats:bgp-state')
-        self.log.info('result dir : ', dir(result))
-        self.log.info('result: ', result)
+        node_path = f"/ncs:devices/device{{{input.device}}}/live-status/ios-stats:bgp/ipv4/unicast/neighbors{{{input.bgp_neighbor_addr}}}/ios-stats:bgp-state"
+        self.log.info('node_path: ', node_path)
+        result = ncs.maagic.get_node(trans, node_path)
+        self.log.info('bgp state result: ', result)
         # Set the output which is returned with Action is run.
         output.bgp_state = result
 
